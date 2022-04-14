51bb76c6c4d898b38748ab49dccb3e4c9828dd87
weronikasos <agnieszkasoszka99@gmail.com>
2022-04-14 16:14:23 +0200

    how to change name/age in all places

---
+++ string/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+string characterName="John" ;
+int characterAge=35;
+
+int main()
+{
+    cout << ""<<characterName<< " was his name "<< endl;
+    cout << "He was "<<characterAge<< " years old"<< endl;
+    return 0;
+}
