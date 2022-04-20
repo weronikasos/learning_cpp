caf501fdf6cfeb0ec8bed4d0585bd0745a24218b
weronikasos <agnieszkasoszka99@gmail.com>
2022-04-20 22:16:20 +0200

    basic equations

---
+++ types_of_numbers/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+
+{
+    int wnum = 5;
+    double dnum = 5.5 ;
+    cout << 10 % 3 << endl;
+    cout << 5+ 4 *10 << endl;
+    cout <<(5+ 4)*10 << endl;
+    wnum ++;
+    cout << wnum << endl;
+    wnum --;
+    cout << wnum <<endl;
+    cout << 10.0 /3.0 <<endl;
+    cout << 10 /3 << endl;
+    cout << pow 2 , 5)<<endl;
+    cout << sqrt(36) <<endl;
+    cout << round 5.2) <<endl;
+    cout << round 5.6) <<endl;
+    cout << ceil(2.1) <<endl;
+    cout << floor(3.8) <<endl;
+    cout << fmax (6,9) <<endl;
+
+
+
+      return 0;
+}
